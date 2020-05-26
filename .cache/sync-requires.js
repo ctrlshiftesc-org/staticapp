e412b20469520fcb69d619f21bd4fa0909aed405
Lance Hughes <tyler.hughes@chaostechops.com>
2020-05-25 20:28:20 -0400

    added additional scripts to packages file

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/lancehughes/Projects/newwebsite/.cache/dev-404-page.js"))),
   "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": hot(preferDefault(require("/Users/lancehughes/Projects/newwebsite/node_modules/@wkocjan/gatsby-theme-intro/src/pages/404.js"))),
   "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js": hot(preferDefault(require("/Users/lancehughes/Projects/newwebsite/node_modules/@wkocjan/gatsby-theme-intro/src/templates/index.js")))
 }
