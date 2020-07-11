04efc373a871ebc5a5f827a3faa5b90c15c75ec4
Lance Hughes <lhughes@chaostechops.com>
2020-07-11 12:21:28 -0400

    Local Gatsby build

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,5 +1,5 @@
 var plugins = [{
-      plugin: require('/Users/lancehughes/Projects/newwebsite/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/lancehughes/Documents/GitHub/staticapp/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
